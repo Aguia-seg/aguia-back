fd22d174f65499ef631a5fbfd7c554b634097bbb
Herbet Junior <herbetjr@gmail.com>
2023-03-13 18:16:47 -0300

    dsdsa

--- app/Http/Controllers/Api/ClientsController.php
+++ app/Http/Controllers/Api/ClientsController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreClientPost;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Http\RedirectResponse;
 use App\Models\Client;
 use App\Models\Contract;
 use App\Models\Invoice;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Validator;
 
 class ClientsController extends Controller
 {
@@ -27,32 +30,49 @@ class ClientsController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): Response
+    public function store(StoreClientPost $request): RedirectResponse
     {
+        // $messages = [
+        //     'unique' => 'Já existe :attribute cadastrado.',
+        // ];
+
+        // $validator = Validator::make($request->all(), [
+        //     'email' => 'unique:clients',
+        // ], $messages);
+
+        // if ($validator->fails()) {
+        //     return response($validator->errors(), 400);
+        // }
+
+        $validator = $request->validated();
+
+        // if ($validator->fails()) {
+        //     return response($validator->errors(), 400);
+        // }
         $client = Client::create($request->all());
 
         if ($client) {
-            $contract = Contract::create([
-                'client_id' => $client->id,
-                'plan_id' => $request->plano['id'],
-                'expiration' => Carbon::create(null, 12, $request->payday),
-                'payday' => $request->payday,
-                'value' => $request->plano['value']
-            ]);
+            // $contract = Contract::create([
+            //     'client_id' => $client->id,
+            //     'plan_id' => $request->plano['id'],
+            //     'expiration' => Carbon::create(null, 12, $request->payday),
+            //     'payday' => $request->payday,
+            //     'value' => $request->plano['value']
+            // ]);
 
-            if($contract){
-                $data_atual = Carbon::now();
-                $mes_atual = $data_atual->month;
-                for ($i = $mes_atual; $i <= 12; $i++) { 
-                    $invoices = Invoice::create([
-                        'client_id' => $client->id,
-                        'contract_id' => $contract->id,
-                        'expiration' => Carbon::create(null, $i, $request->payday),
-                        'value' => $request->plano['value']
-                    ]);
-                }              
+            // if($contract){
+            //     $data_atual = Carbon::now();
+            //     $mes_atual = $data_atual->month;
+            //     for ($i = $mes_atual; $i <= 12; $i++) { 
+            //         $invoices = Invoice::create([
+            //             'client_id' => $client->id,
+            //             'contract_id' => $contract->id,
+            //             'expiration' => Carbon::create(null, $i, $request->payday),
+            //             'value' => $request->plano['value']
+            //         ]);
+            //     }              
 
-            }
+            // }
 
             return response([
                 'message' => 'Cliente cadastrado com sucesso'
@@ -72,7 +92,7 @@ class ClientsController extends Controller
         );
     }
 
-  
+
 
     /**
      * Update the specified resource in storage.
@@ -90,13 +110,13 @@ class ClientsController extends Controller
         //
     }
 
-      public function search(Request $request): Response
+    public function search(Request $request): Response
     {
-        $client = Client::where('name', 'LIKE',"%{$request->search}%" )
-        ->orWhere('cpf_cnpj','=',$request->search)
-        ->get();
+        $client = Client::where('name', 'LIKE', "%{$request->search}%")
+            ->orWhere('cpf_cnpj', '=', $request->search)
+            ->get();
         return response(
-           $client,
+            $client,
             // 'contracts' => $client->contracts
         );
     }
