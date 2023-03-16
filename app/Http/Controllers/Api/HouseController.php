48531c32b9af62899e526e653703180c481ce09e
João Vitor <joao.vst003@gmail.com>
2023-03-16 18:45:15 -0300

    asdasdas

---
+++ app/Http/Controllers/Api/HouseController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+
+class HouseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): Response
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): Response
+    {
+        
+    }
+
+    public function showDistinct(): Response
+    {
+        $district = DB::table('houses')->distinct()->get(['district']);
+
+        return response(
+            $houses
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): Response
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): Response
+    {
+        //
+    }
+}
