b09b3af017fc22fd64a775ced03292f430f2b4cd
João Vitor <joao.vst003@gmail.com>
2023-03-28 16:56:59 -0300

    asda

--- routes/house/house_routes.php
+++ routes/house/house_routes.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\Api\HouseController;
 
 Route::get('houses', [HouseController::class, 'index']);
 Route::get('houses/district', [HouseController::class, 'showDistinctDistrict']);
-Route::get('houses/street', [HouseController::class, 'showDistinctStreet']);
+Route::get('houses/street/{district}', [HouseController::class, 'showDistinctStreet']);
