f1f73b4d24fae3d3c97b5d116d06354938bc0a10
João <joao.vst003@gmail.com>
2023-04-05 18:11:44 -0300

    asdas

--- app/Http/Controllers/Api/HouseController.php
+++ app/Http/Controllers/Api/HouseController.php
@@ -24,15 +24,13 @@ class HouseController extends Controller
     public function filter(Request $request): Response
     {
        
-        $houses = House::where('city', 'LIKE', "%$request->city%")
+        $houses = House::join('clients', 'houses.id', '=', 'clients.id')->where('city', 'LIKE', "%$request->city%")
         ->where('district', 'LIKE', "%$request->district%")
         ->where('street', 'LIKE', "%$request->street%")
         ->get();
         
         return response(
-           [
-            'testes' => $houses
-           ]
+           $houses
         );
     }
 
